158652add31e36c5be1fd375ef88624efbc27aec
WidadAnncoud <widad.anncoud@student.ehb.be>
2022-05-02 11:19:11 +0200

    puzzle1 af gemaakt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,30 @@
 fun main(args: Array<String>) {
-    val reader = FileReader();
 
-    println(reader.getPuzzle1())
+    fun puzzle1(){
+        val reader = FileReader();
+        val puzzle = reader.getPuzzle1()
+
+        var previous = puzzle[0]
+        var increases = 0
+
+        puzzle.forEach{
+            if (it > previous){
+                increases++
+            }
+            previous = it
+        }
+        println(increases)
+    }
+
+    puzzle1()
     //println(reader.getPuzzle2())
     //println(reader.getPuzzle3())
     //println(reader.getPuzzle4Numbers())
     //println(reader.getPuzzle4Cards())
 }
+
+
+//vorig getal bijhouden
+//volgend ophalen
+//bijde vergelijken
+//if cuurent is klijner dan next increase++
